6b00051c1a930d0968c0496e56fc0487d1e68344
gelauft <gelauft.verwaltung@gmail.com>
2023-02-05 21:52:41 +0100

    conformation system

--- src/main/java/de/gelauft/navy/discord/bot/manager/MemberManager.java
+++ src/main/java/de/gelauft/navy/discord/bot/manager/MemberManager.java
@@ -17,7 +17,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
